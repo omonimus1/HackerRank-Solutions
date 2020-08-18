9e93525ee6f2006ecf4220f77121282ff16cd4dd
Davide Pollicino <davidepollicino2015@gmail.com>
2020-08-18 16:16:52 +0100

    Adds circular array rotation solution

--- README.md
+++ README.md
@@ -84,6 +84,7 @@ Here you will be able to find my HackerRank solutions to many challenghes, using
 |Tutorial Intro|[C++](problem-solving/turorial-intro.cpp)||
 |Bon Appetit |[C++](problem-solving/bon-appetit.cpp)||
 |Sock Merchant|[C++](problem-solving/sock-merchant.cpp)||
+| Circular array rotation|[C++](problem-solving/circular-array-rotation.cpp)||
 
 
 
---
+++ problem-solving/circular-array-rotation.cpp
@@ -0,0 +1,115 @@
+// https://www.hackerrank.com/challenges/circular-array-rotation/problem
+// WARNING: This solutions got 12/1000, please do a pull request if you have a better solution
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the circularArrayRotation function below.
+vector<int> circularArrayRotation(vector<int> a, int k, vector<int> queries) {
+    // K is the number of cicles -
+    // For every rottion cycle, the last became the first element and all the other
+    // elements shift forwards
+   int len = a.size();
+   vector<int>solution; 
+   int last; 
+   for(int i=0; i  < k; i++)
+   {
+       last = a[len-1];
+       for(int j = len-1; j >0; j--)
+            a[j] = a[j-1];
+        a[0] = last;
+   }
+
+   for(int i =0; i < queries.size(); i++)
+   {
+       solution.push_back(a[queries[i]]);
+   }
+   return solution; 
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nkq_temp;
+    getline(cin, nkq_temp);
+
+    vector<string> nkq = split_string(nkq_temp);
+
+    int n = stoi(nkq[0]);
+
+    int k = stoi(nkq[1]);
+
+    int q = stoi(nkq[2]);
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split_string(a_temp_temp);
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    vector<int> queries(q);
+
+    for (int i = 0; i < q; i++) {
+        int queries_item;
+        cin >> queries_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        queries[i] = queries_item;
+    }
+
+    vector<int> result = circularArrayRotation(a, k, queries);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
