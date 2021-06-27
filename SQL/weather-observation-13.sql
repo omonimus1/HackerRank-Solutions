c71a128f030b2cf87f8f64144daebd04f2f2af2a
Davide Pollicino <davidepollicino2015@gmail.com>
2021-06-27 11:51:41 +0100

    Adds weather station

---
+++ SQL/weather-observation-13.sql
@@ -0,0 +1,7 @@
+/*
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater 
+than  and less than . Truncate your answer to  decimal places.
+*/
+SELECT ROUND(SUM(LAT_N), 4)
+FROM STATION
+WHERE LAT_N > 38.7880 AND  LAT_N < 137.2345;
