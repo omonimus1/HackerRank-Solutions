546c055eb5ce73cc09f07ecaea2b905e9e649b44
Davide Pollicino <davidepollicino2015@gmail.com>
2025-11-18 16:58:37 +0100

    Update find %

--- python/find-the-percentage.py
+++ python/find-the-percentage.py
@@ -1,6 +1,5 @@
 #  https://www.hackerrank.com/challenges/finding-the-percentage/problem
 if __name__ == '__main__':
-    # Get number of students
     n = int(input())
     student_marks = {}
     for _ in range(n):
@@ -8,11 +7,6 @@ if __name__ == '__main__':
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
-    score_record = []
-    score_record = student_marks[query_name]
-
-    sum_marks = 0
-    for x in score_record:
-        sum_marks += x
-    percentage = "{:.2f}".format(sum_marks/len(score_record))
-    print(percentage)
+    
+    average_marks = sum(student_marks[query_name]) / len(student_marks[query_name])
+    print(f"{average_marks:.2f}")
