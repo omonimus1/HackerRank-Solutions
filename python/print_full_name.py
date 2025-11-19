922076b598054f611f888c61e8ef3d901e4f6035
Davide Pollicino <davidepollicino2015@gmail.com>
2025-11-19 23:52:19 +0100

    Print full name

---
+++ python/print_full_name.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/whats-your-name/problem?isFullScreen=true
+#
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+def print_full_name(first, last):
+    print(f'Hello {first} {last}! You just delved into python.')
