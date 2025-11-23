33820e9a50af835020c0f204e03eac6587483369
Davide Pollicino <davidepollicino2015@gmail.com>
2025-11-23 22:41:07 +0100

    power_mod

---
+++ python/power_mod.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+# print(pow(a, b, m))  (a^b) %M
+a = int(input())
+b = int(input())
+m = int(input())
+print(pow(a,b))
+print(pow(a, b, m))
