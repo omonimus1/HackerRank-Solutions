d8e084ad8d707ffe25f66ff4aa4589a69747fb04
Davide Pollicino <davidepollicino2015@gmail.com>
2025-11-23 22:45:36 +0100

    integer comes in all sizes

---
+++ python/integer_come_in_all_sizes.py
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem?isFullScreen=true
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(pow(a,b) + pow(c,d))
